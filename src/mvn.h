ac600d45d30c968b4598229d1a39d6c175b78609
Carl Boettiger <cboettig@gmail.com>
2011-04-22 13:19:25 -0700

    added header for mvn.h

---
+++ src/mvn.h
@@ -0,0 +1,24 @@
+/**
+ * @file mvn.h
+ * @author Carl Boettiger <cboettig@gmail.com>
+ * @date 22 April 2011
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+int mvn(const gsl_rng * rng, const gsl_vector * mean, gsl_matrix * covar, gsl_vector * ANS);
+
