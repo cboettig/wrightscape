cde15cf869d58a1895e5bec6ad9b7699c86ed51b
Carl Boettiger <cboettig@gmail.com>
2011-04-27 16:14:24 -0700

    intramandibular joint comparison

--- demos/model_fits.R
+++ demos/model_fits.R
@@ -10,6 +10,11 @@ labrid_tree <- read.nexus(paste(path, "labrid_tree.nex", sep=""))
 fin_data <-read.csv(paste(path,"labrid.csv", sep=""))
 diet_data <- read.csv(paste(path,"labriddata_parrotfish.csv", sep=""))
 
+# Select common ancestor of a Chlorurus and a Hipposcarus as the changepoint
+intramandibular <- paintBranches(mrcaOUCH(c("Chlorurus_sordidus", "Hipposcarus_longiceps"), labrid$tree), labrid$tree, c("other","intramandibular"))
+
+
+
 # size correct length and weight by body mass
 for(i in c(3,4,6,7,8)){
 	diet_data[i] <- diet_data[i]/diet_data[5]
@@ -27,13 +32,24 @@ i <- 3
 	bm <- brown(trait, labrid$tree)
 	ou1 <- hansen(trait, labrid$tree, regime=labrid$noregimes, .01, .01)
 	ou2 <- hansen(trait, labrid$tree, regime=labrid$regimes, .01, .01)
-	ws2 <- wrightscape(trait, labrid$tree, regime=labrid$regimes, (ou2@sqrt.alpha)^2, ou2@sigma, theta=ou2@theta[[1]])
 
 
 ## needs to be fixed, singular lik problems?
-    ouch_test <- ouch(trait, labrid$tree, regime=labrid$regimes, alpha=(ou2@sqrt.alpha)^2, sigma=ou2@sigma)
-	#a <- simulate(ouch_test)
+  ouch_test <- ouch(trait, labrid$tree, regime=labrid$regimes, alpha=(ou2@sqrt.alpha)^2, sigma=ou2@sigma)
+	a <- simulate(ouch_test)
 	# update(ouch_test, a)
+
+
+
+  ws2_phar <- wrightscape(trait, labrid$tree, regime=labrid$regimes, (ou2@sqrt.alpha)^2, ou2@sigma, theta=ou2@theta[[1]])
+	ou2 <- hansen(trait, labrid$tree, regime=intramandibular, .01, .01)
+  ws2_intra <- wrightscape(trait, labrid$tree, regime=intramandibular, (ou2@sqrt.alpha)^2, ou2@sigma, theta=ou2@theta[[1]])
+
+loglik(ws2_par)
+loglik(ws2_intra)
+
+#  pharyngeal vs intramnadibular regimes
+
 
 	brownie_test <- brownie(trait, labrid$tree, regime=labrid$regimes, sigma=ou2@sigma)
 	a <- simulate(brownie_test)
