cd8eab96fd1d1bedabe90da25a36897d27ed1734
Carl Boettiger <cboettig@gmail.com>
2011-06-08 11:54:17 -0700

    parameter distributions for bootstraps

--- demo/simulated_release.R
+++ demo/simulated_release.R
@@ -33,33 +33,72 @@ gen_fit <- multiTypeOU(data=sim_trait, tree=labrid$tree,
                   method ="SANN", control=list(maxit=80000,temp=25,tmax=50))
 
 save(list=ls(), file="simtest.Rdat")
-require(pmc)
 
+
+require(pmc)
 sfInit(parallel=T, cpu=16)
 sfLibrary(wrightscape)
 sfExportAll()
-boots <- montecarlotest(fit, gen_fit, nboot=80, cpu=16, GetParNames=FALSE)
+
+boots <- montecarlotest(fit, gen_fit, nboot=800, cpu=16, GetParNames=TRUE)
 social_plot(plot(boots), tags="phylogenetics")
 
-sfStop()
-sfInit(parallel=T, cpu=4)
-sfLibrary(wrightscape)
-sfExportAll()
-o <- phylo_mcmc(sim_trait, labrid$tree, intramandibular, MaxTime=1e5, indep=1e2, nchains=4)
 
-png("alphadist.png")
-cold_chain <- o$chains[[1]]
-colnames(cold_chain) <- c("Pi", "Xo", "alpha1", "alpha2", "sigma", "theta")
-poste_alpha1 <- density(cold_chain[, "alpha1"])
-poste_alpha2 <- density(cold_chain[, "alpha2"])
+####### Plot the distributions
+rownames(boots$test_par_dist) <- names(getParameters(gen_fit))
+par_dist <- t(boots$test_par_dist) 
+
+social_plot({
+par(mfrow=c(1,3))
+poste_alpha1 <- density(par_dist[, "alpha1"])
+poste_alpha2 <- density(par_dist[, "alpha2"])
 xlim <- c(min(poste_alpha1$x, poste_alpha2$x), max(poste_alpha1$x, poste_alpha2$x)) 
 ylim <- c(min(poste_alpha1$y, poste_alpha2$y), max(poste_alpha1$y, poste_alpha2$y)) 
-plot(poste_alpha2, xlab="alpha", main="Selection Strength", xlim=xlim, ylim=ylim)
+plot(poste_alpha2, xlab="alpha", main="Selection Strength", xlim=xlim, ylim=ylim, cex=2, cex.lab=2, cex.main=2, cex.axis=2)
 polygon(poste_alpha1, col=rgb(0,1,0,.5))
 polygon(poste_alpha2, col=rgb(0,0,1,.5))
-dev.off()
 
-social_report(file="alphadist.png", tag="phylogenetics")
+poste_theta1 <- density(par_dist[, "theta1"])
+poste_theta2 <- density(par_dist[, "theta2"])
+xlim <- c(min(poste_theta1$x, poste_theta2$x), max(poste_theta1$x, poste_theta2$x))
+ylim <- c(min(poste_theta1$y, poste_theta2$y), max(poste_theta1$y, poste_theta2$y)) 
+plot(poste_theta2, xlab="theta", main="Optimum", xlim=xlim, ylim=ylim, cex=2, cex.lab=2, cex.main=2, cex.axis=2)
+polygon(poste_theta1, col=rgb(0,1,0,.5))
+polygon(poste_theta2, col=rgb(0,0,1,.5))
+
+poste_sigma1 <- density(par_dist[, "sigma1"])
+poste_sigma2 <- density(par_dist[, "sigma2"])
+xlim <- c(min(poste_sigma1$x, poste_sigma2$x), max(poste_sigma1$x, poste_sigma2$x)) 
+ylim <- c(min(poste_sigma1$y, poste_sigma2$y), max(poste_sigma1$y, poste_sigma2$y)) 
+plot(poste_sigma2, xlab="sigma", main="Diversification rate", xlim=xlim, ylim=ylim, cex=2, cex.lab=2, cex.main=2, cex.axis=2)
+polygon(poste_sigma1, col=rgb(0,1,0,.5))
+polygon(poste_sigma2, col=rgb(0,0,1,.5))
+}, file="parameter_boostraps.png", width=3*480, tag="phylogenetics")
+
+
+rownames(boots$null_par_dist) <- names(getParameters(fit))
+par_dist <- t(boots$null_par_dist)
+social_plot({
+poste_alpha1 <- density(par_dist[, "alpha1"])
+poste_alpha2 <- density(par_dist[, "alpha2"])
+xlim <- c(min(poste_alpha1$x, poste_alpha2$x), max(poste_alpha1$x, poste_alpha2$x)) 
+ylim <- c(min(poste_alpha1$y, poste_alpha2$y), max(poste_alpha1$y, poste_alpha2$y)) 
+plot(poste_alpha2, xlab="alpha", main="Selection Strength", xlim=xlim, ylim=ylim, cex=2, cex.lab=2, cex.main=2, cex.axis=2)
+polygon(poste_alpha1, col=rgb(0,1,0,.5))
+polygon(poste_alpha2, col=rgb(0,0,1,.5))
+}, file="parameter_boostraps.png", width=480, tag="phylogenetics")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
