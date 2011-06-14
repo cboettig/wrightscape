a576b842d4403cd8bd1065c835a7a8840c8df4d3
Carl Boettiger <cboettig@gmail.com>
2011-06-14 13:00:36 -0700

    parrotfish_gape.R  autocommit

--- demo/parrotfish_gape.R
+++ demo/parrotfish_gape.R
@@ -14,12 +14,12 @@ tweet_errors(script, gitopts, tags)  ## tweet on error
 
 source("parrotfish_data.R")
 
-alphas <- multiTypeOU(data=labrid$data["close"], tree=labrid$tree, regimes=intramandibular, 
+alphas <- multiTypeOU(data=labrid$data["prot.y"], tree=labrid$tree, regimes=intramandibular, 
                   model_spec=list(alpha="indep", sigma="global", theta="indep"), 
                   Xo=NULL, alpha = .1, sigma = .1, theta=NULL,
                   method ="SANN", control=list(maxit=50000,temp=50,tmax=20))
 
-sigmas <- multiTypeOU(data=labrid$data["close"], tree=labrid$tree, regimes=intramandibular, 
+sigmas <- multiTypeOU(data=labrid$data["prot.y"], tree=labrid$tree, regimes=intramandibular, 
                 model_spec=list(alpha="global", sigma="indep", theta="indep"), 
                   Xo=NULL, alpha = .1, sigma = .1, theta=NULL,
                   method ="SANN", control=list(maxit=50000,temp=50,tmax=20))
@@ -31,7 +31,7 @@ sfInit(parallel=TRUE, cpu=8)
 sfLibrary(wrightscape)
 sfExportAll()
 
-boots <- montecarlotest(sigmas, alphas, nboot=800, cpu=16)
+boots <- montecarlotest(sigmas, alphas, nboot=400, cpu=8)
 png("sigmas_v_alphas.png")
 plot(boots)
 dev.off()
