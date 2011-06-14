ad87aa51893c4f88f3e4e76a9b6e4bb4ce915b20
Carl Boettiger <cboettig@gmail.com>
2011-06-14 09:22:44 -0700

    mcmc_demo.R  autocommit

--- demo/mcmc_demo.R
+++ demo/mcmc_demo.R
@@ -9,7 +9,7 @@ on.exit(system("git push")) #  For git links.  May prompt for pw,
 tweet_errors(script, gitopts, tags)  ## tweet on error
 
 source("parrotfish_data.R")
-MaxTime = 1e7
+MaxTime = 1e6 # 1e7 too great to store in mem, better start writing to file!
 spec = list(alpha="indep", sigma="global", theta="global")
 
 comment="open"
