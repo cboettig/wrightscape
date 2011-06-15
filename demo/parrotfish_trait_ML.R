061b7715677356d604ac7072ee5e9f55fc113ad2
Carl Boettiger <cboettig@gmail.com>
2011-06-14 22:57:50 -0700

    parrotfish_trait_ML.R  autocommit

--- demo/parrotfish_trait_ML.R
+++ demo/parrotfish_trait_ML.R
@@ -16,12 +16,12 @@ tweet_errors(script, gitopts, tags)
 source("parrotfish_data.R")
 
 alphas <- multiTypeOU(data=labrid$data["prot.y"], tree=labrid$tree, regimes=intramandibular, 
-                  model_spec=list(alpha="indep", sigma="global", theta="indep"), 
+                  model_spec=list(alpha="indep", sigma="global", theta="global"), 
                   Xo=NULL, alpha = .1, sigma = .1, theta=NULL,
                   method ="SANN", control=list(maxit=50000,temp=50,tmax=20))
 
 sigmas <- multiTypeOU(data=labrid$data["prot.y"], tree=labrid$tree, regimes=intramandibular, 
-                model_spec=list(alpha="global", sigma="indep", theta="indep"), 
+                model_spec=list(alpha="global", sigma="indep", theta="global"), 
                   Xo=NULL, alpha = .1, sigma = .1, theta=NULL,
                   method ="SANN", control=list(maxit=50000,temp=50,tmax=20))
 
@@ -39,7 +39,7 @@ dev.off()
 
 upload("sigmas_v_alphas.png", script=script, tags=tags, gitaddr=gitaddr)
 
-## this needs to become a smart function
+## this does work without adjusting
 finalplots <- function(boots){
   par_dist <- t(boots$test_par_dist) 
 
