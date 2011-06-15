c61de6c39e20f5e1c0806a415be3c96275193097
Carl Boettiger <cboettig@gmail.com>
2011-06-15 14:29:55 -0700

    parrotfish_trait_mcmc.R  autocommit

--- demo/parrotfish_trait_mcmc.R
+++ demo/parrotfish_trait_mcmc.R
@@ -17,27 +17,29 @@ source("parrotfish_data.R")
 nchains <- 8
 MaxTime = 1e6 # 1e7 too great to store in mem, better start writing to file!
 spec = list(alpha="indep", sigma="global", theta="global")
-traits <- c("close", "open", "gape.y", "prot.y")
+traits <- c("prot.y", "close", "open", "gape.y")
 
-#for(trait in traits){
-  sfInit(parallel=T, cpu=8)
-  sfLibrary(wrightscape)
-  sfExportAll()
+sfInit(parallel=T, cpu=4)
+sfLibrary(wrightscape)
+sfExportAll()
+sfLapply(traits, function(trait){
 
-
-  o <- sfLapply(1:nchains, function(i){ 
-  o <- phylo_mcmc(labrid$data['gape.y'], labrid$tree, intramandibular,
+#  o <- sfLapply(1:nchains, function(i){ 
+  chains <- phylo_mcmc(labrid$data[trait], labrid$tree, intramandibular,
                   MaxTime=MaxTime, model_spec=spec, stepsizes=0.05)[[1]]
-      })
+#      })
 
   burnin <- 1:1e3
-  chains <- o[[1]][-burnin,]
-  for(i in 2:nchains)
-    chains <- rbind(chains, o[[i]][-burnin, ])
+  chains <- chains[-burnin,]
+
+#  chains <- o[[1]][-burnin,]
+#  for(i in 2:nchains)
+#    chains <- rbind(chains, o[[i]][-burnin, ])
 
   png(file="parameter_mcmc.png", width=3*480)
   plot.phylo_mcmc(chains, cex=3, cex.lab=3, cex.main=3, cex.axis=3)
   dev.off()
-  upload("parameter_mcmc.png", script, gitaddr=gitaddr, tags=tags)
+  upload("parameter_mcmc.png", script, gitaddr=gitaddr, 
+          tags=tags, comment=trait)
 
-#}
+})
