27cbf27dd5d957788a03cf447ace7cbf82dc959f
Carl Boettiger <cboettig@gmail.com>
2011-06-15 14:20:35 -0700

    sim_mcmc.R  autocommit

--- demo/sim_mcmc.R
+++ demo/sim_mcmc.R
@@ -11,10 +11,7 @@ tweet_errors(script, gitopts, tags)  ## tweet on error
 
 source("parrotfish_data.R")
 # since we can't install package while other reps are running
-source("../R/wrightscape.R")
-source("../R/mcmc.R")
-source("../R/likelihood.R")
-source("../R/prior_library.R")
+
 # Create some simulated data on the parrotfish tree
 true <- multiTypeOU(
         data=labrid$data["close"], tree=labrid$tree,regimes=intramandibular, 
@@ -25,31 +22,25 @@ true$alpha <- c(.01, 20)
 true$sigma <- 10
 true$theta <- 0
 sim_trait <- simulate(true, seed=1)
-## Start with a simple fit of indep alphas model to get some parameters
-## of course we could also start with the true values.  
-#fit <- multiTypeOU(data=sim_trait, tree=labrid$tree, regimes=intramandibular, 
-#                model_spec=list(alpha="indep", sigma="global", theta="global"), 
-#                Xo=NULL, alpha = .1, sigma = .1, theta=NULL,
-#                  method ="SANN", control=list(maxit=80000,temp=25,tmax=50)) 
 
-nchains<-8
-sfInit(parallel=T, cpu=8)
-sfLibrary(wrightscape)
-sfExportAll()
+#nchains<-8
+#sfInit(parallel=T, cpu=8)
+#sfLibrary(wrightscape)
+#sfExportAll()
 # MCMCMC the rc model
 
-o <- sfLapply(1:nchains, function(i){ 
-    phylo_mcmc(sim_trait, labrid$tree, intramandibular, 
+#o <- sfLapply(1:nchains, function(i){ 
+ chains<-   phylo_mcmc(sim_trait, labrid$tree, intramandibular, 
                MaxTime=1e5, alpha=true$alpha, sigma=true$sigma, 
                theta=true$theta, Xo=true$Xo,
                model_spec=list(alpha="indep", sigma="global", theta="global"),
-               stepsizes=0.5)[[1]]
-    })
+               stepsizes=0.05)[[1]]
+   # })
 
-burnin <- 1:1e3
-chains <- o[[1]][-burnin,]
-for(i in 2:nchains)
-  chains <- rbind(chains, o[[i]][-burnin, ])
+#burnin <- 1:1e3
+#chains <- o[[1]][-burnin,]
+#for(i in 2:nchains)
+#  chains <- rbind(chains, o[[i]][-burnin, ])
 
 
 png(file="parameter_mcmc.png", width=3*480)
