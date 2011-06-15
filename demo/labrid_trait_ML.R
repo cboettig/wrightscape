8e26d3bf4f872039817ba5916894ff9d834b1216
Carl Boettiger <cboettig@gmail.com>
2011-06-14 22:58:20 -0700

    labrid_trait_ML.R  autocommit

--- demo/labrid_trait_ML.R
+++ demo/labrid_trait_ML.R
@@ -15,12 +15,12 @@ tweet_errors(script, gitopts, tags)
 source("labrid_data.R")
 
 alphas <- multiTypeOU(data=labrid$data["prot.y"], tree=labrid$tree, regimes=pharyngeal, 
-            model_spec=list(alpha="indep", sigma="global", theta="indep"), 
+            model_spec=list(alpha="indep", sigma="global", theta="global"), 
             Xo=NULL, alpha = .1, sigma = .1, theta=NULL,
             method ="SANN", control=list(maxit=50000,temp=50,tmax=20))
 
 sigmas <- multiTypeOU(data=labrid$data["prot.y"], tree=labrid$tree, regimes=pharngeal, 
-                model_spec=list(alpha="global", sigma="indep", theta="indep"), 
+                model_spec=list(alpha="global", sigma="indep", theta="global"), 
                   Xo=NULL, alpha = .1, sigma = .1, theta=NULL,
                   method ="SANN", control=list(maxit=50000,temp=50,tmax=20))
 
