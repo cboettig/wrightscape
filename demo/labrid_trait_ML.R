dbcfcd0ed6ae8387708f12a6251b8e5664809748
Carl Boettiger <cboettig@gmail.com>
2011-06-16 14:55:07 -0700

    labrid_trait_ML.R  autocommit

--- demo/labrid_trait_ML.R
+++ demo/labrid_trait_ML.R
@@ -21,7 +21,7 @@ alphas <- multiTypeOU(data=labrid$data["prot.y"], tree=labrid$tree, regimes=phar
             method ="SANN", control=list(maxit=50000,temp=50,tmax=20))
 
 sigmas <- multiTypeOU(data=labrid$data["prot.y"], tree=labrid$tree, regimes=pharyngeal, 
-                model_spec=list(alpha="global", sigma="indep", theta="global"), 
+                model_spec=list(alpha="fixed", sigma="indep", theta="global"), 
                   Xo=NULL, alpha = .1, sigma = .1, theta=NULL,
                   method ="SANN", control=list(maxit=50000,temp=50,tmax=20))
 
