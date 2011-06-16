16c8e442989bb081d5f0d9ca389a08cb52af973d
Carl Boettiger <cboettig@gmail.com>
2011-06-16 11:21:15 -0700

    labrid_trait_mcmc.R  autocommit

--- demo/labrid_trait_mcmc.R
+++ demo/labrid_trait_mcmc.R
@@ -14,10 +14,12 @@ tweet_errors(script, gitopts, tags)  ## tweet on error
 
 source("labrid_data.R")
 
-nchains <- 8
 MaxTime = 1e6 
-spec = list(alpha="indep", sigma="global", theta="global")
+spec = list(alpha="indep", sigma="indep", theta="global")
 traits <- c("prot.y", "close", "open", "gape.y")
+
+nchains <- 4
+burnin <- 1:1e5
 
 
 sfInit(parallel=T, cpu=4)
@@ -25,20 +27,30 @@ sfLibrary(wrightscape)
 sfLibrary(socialR)
 sfExportAll()
 
-sfLapply(traits, function(trait){
+#sfLapply(traits, function(trait){
   # START SMART PLEASE
   start <- multiTypeOU(data=labrid$data[trait], tree=labrid$tree, 
                   regimes=pharyngeal, model_spec=spec,
                   method ="SANN", control=list(maxit=100000,temp=50,tmax=20))
 
-  chains <- phylo_mcmc(labrid$data[trait], labrid$tree, pharyngeal,
-                       MaxTime=MaxTime, model_spec=spec, stepsizes=0.05,
-                       Xo=start$Xo, alpha=start$alpha, sigma=start$sigma,
-                       theta=start$theta)
-  # returns [[1]]: chains, [[2]]: myCall, [[3]] colnames (for txtfile version)
+  o <- sfLapply(1:4, function(i)){
+    chains <- phylo_mcmc(labrid$data[trait], labrid$tree, pharyngeal,
+                         MaxTime=MaxTime, model_spec=spec, stepsizes=0.05,
+                         Xo=start$Xo, alpha=start$alpha, sigma=start$sigma,
+                         theta=start$theta)
+    # returns [[1]]: chains, [[2]]: myCall, [[3]] colnames (for txtfile version)
+  chains[[1]]
+  }
 
-  burnin <- 1:1e5
-  chains <- chains[[1]][-burnin,]
+  # the first chain
+  chains <- o[[1]][-burnin,]
+  for(i in 2:nchains){
+    chains <- rbind(chains, o[[i]][-burnin, ]
+  }
+
+  # chains <- chains[[1]][-burnin,]  ## without parrallel
+
+
   png(file="parameter_mcmc.png", width=3*480)
   plot.phylo_mcmc(chains, cex=3, cex.lab=3, cex.main=3, cex.axis=3)
   dev.off()
