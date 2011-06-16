e07b1b5b4e4ccedde29140f7257c12100985efb6
Carl Boettiger <cboettig@gmail.com>
2011-06-15 18:06:30 -0700

    plotting and mcmc changes

--- demo/mcmc_test.R
+++ demo/mcmc_test.R
@@ -2,8 +2,8 @@
 rm(list=ls())
 require(wrightscape)
 
-source("../R/mcmc.R")
-source("../R/likelihood.R")
+#source("../R/mcmc.R")
+#source("../R/likelihood.R")
 source("parrotfish_data.R")
 
 MaxTime = 1e3 # 1e7 too great to store in mem, better start writing to file!
@@ -11,9 +11,12 @@ spec = list(alpha="global", sigma="indep", theta="global")
 
 ## uses [[1]] to return chains only, doesn't return the myCall
 o <- phylo_mcmc(labrid$data['prot.y'], labrid$tree, intramandibular,
-                MaxTime=MaxTime, model_spec=spec, stepsizes=0.05)[[1]]
+                MaxTime=MaxTime, model_spec=spec, stepsizes=0.05, 
+                write="mcmc_out.txt")
 
-plot.phylo_mcmc(o, cex=3, cex.lab=3, cex.main=3, cex.axis=3)
+chain <- read.csv(o[[1]], sep=" ")
+colnames(chain) <- o[[3]]
+plot.phylo_mcmc(chain, cex=3, cex.lab=3, cex.main=3, cex.axis=3)
 
 
 
