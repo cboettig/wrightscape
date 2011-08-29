23b017f7187485fe22304de117e0e3b8347a29ed
Carl Boettiger <cboettig@gmail.com>
2011-08-29 00:05:58 -0700

    parrotfish.R  autocommit

--- demo/parrotfish.R
+++ demo/parrotfish.R
@@ -13,6 +13,8 @@ script <- "parrotfish.R"
 tags="phylogenetics wrightscape labrids"
 gitopts <- list(user = "cboettig", dir = "demo", repo = "wrightscape") 
 gitaddr <- gitcommit(script, gitopts)
+hash <- gitlog()$commitID
+print(hash)
 ####################################################
 
 source("parrotfish_data.R")
@@ -25,7 +27,7 @@ alphas <- multiTypeOU(data=labrid$data["prot.y"], tree=labrid$tree,
                   method ="SANN", control=list(maxit=100000,temp=50,tmax=20))
 
 sigmas <- multiTypeOU(data=labrid$data["prot.y"], tree=labrid$tree, regimes=intramandibular, 
-                model_spec=list(alpha="global", sigma="global", theta="global"), 
+                model_spec=list(alpha="fixed", sigma="indep", theta="global"), 
                   Xo=NULL, alpha = .1, sigma = 40, theta=NULL,
                   method ="SANN", control=list(maxit=100000,temp=50,tmax=20))
 
@@ -46,14 +48,16 @@ B_sim <- parLapply(cluster, 1:nboot, function(x)
                    compare_models(B, A))
 stopCluster(cluster)
 mc <- collect(A_sim, B_sim, A, B)
-save(list=ls(), file="parrotfish.Rdat")
+save(list=ls(), file=paste(hash, ".Rdat"))
 
 
 
 ### commands reporting this run from head node; wll link to this code ###
-# require(socialR); require(wrightscape); load("parrotfish.Rdat")
+# require(socialR); require(wrightscape); load(paste(hash, ".Rdat"))
 # png("parrotfish.png"); plot(mc); dev.off()
-# upload("parrotfish.png", script=script, gitaddr=gitaddr, tags=tags)
+# png("Apars.png"); plot_pars(mc$null_par_dist); dev.off()
+# png("Bpars.png"); plot_pars(mc$test_par_dist); dev.off()
+# upload(c("parrotfish.png", "Apars.png", "Bpars.png"),script,git=gitaddr,tag=tags)
 
 
 
