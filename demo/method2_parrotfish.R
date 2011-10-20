e692a962242b1525b92c85b76a5609fcd1c8b596
Carl Boettiger <cboettig@gmail.com>
2011-10-20 13:44:53 -0700

    method2_tools testing

--- demo/method2_parrotfish.R
+++ demo/method2_parrotfish.R
@@ -1,9 +1,6 @@
 # ouwie_parrotfish.R
-# 
 # Author: Carl Boettiger <cboettig@gmail.com>
 # Date: 19 October 2011
-#
-# 
 
 
 #=============================================================================#
@@ -11,9 +8,6 @@
 #=============================================================================#
 require(phytools)
 require(geiger)
-require(maticce)
-require(wrightscape) # just for the format_data() function
-require(pmc) # for the convert() function
 
 # This data has not been released
 path = "../data/labrids/"
@@ -26,7 +20,8 @@ diet_data <- read.csv(paste(path,"labriddata_parrotfish.csv", sep=""))
 #=============================================================================#
 corrected_data <- diet_data
 # Use the simple names from Price et al 2010
-traitnames <- c("Species", "group", "gape", "prot", "bodymass", "AM", "SH", "LP", "close", "open", "kt")
+traitnames <- c("Species", "group", "gape", "prot", "bodymass",
+                "AM", "SH", "LP", "close", "open", "kt")
 names(corrected_data) <- traitnames
 # Lengths are log transformed 
 corrected_data[["gape"]] <- log(corrected_data[["gape"]])
@@ -43,7 +38,8 @@ corrected_data[["LP"]] <- log(corrected_data[["LP"]])/3
 parrotfish <- corrected_data[corrected_data[,2]=="parrotfish",]
 ape <- treedata(labrid_tree, parrotfish[,3:11], parrotfish[,1])
 
-out <- phyl.resid(ape$phy, ape$data[,"bodymass"], ape$data[,c("gape", "prot","AM", "SH", "LP")] )
+out <- phyl.resid(ape$phy, ape$data[,"bodymass"],
+                  ape$data[,c("gape", "prot","AM", "SH", "LP")] )
 ## phyl.resid changes order of species listing. Merge for a set of uncorrected and corrected traits.  
 traits <- merge(ape$data, out$resid, by="row.names")
 # columns that are transformed now have gape.x for untransformed, gape.y for transformed.  
@@ -52,6 +48,13 @@ traits <- merge(ape$data, out$resid, by="row.names")
 #================================================================================#
 # Assign regimes                                                                 #
 #================================================================================#
+
+
+#' A function that returns the edge numbers of all branches descended from
+#' the mrca of sp1 and sp2.  
+#' @param phy a phylogenetic tree of class phylo (ape)
+#' @param sp1 the tip.label of the first species on the tree to be used
+#' @param sp1 the tip.label of the second species on the tree to be used
 get_clade <- function(phy, sp1, sp2){
   M <- mrca(phy)
   who <- M[sp1, sp2]
@@ -66,10 +69,12 @@ get_clade <- function(phy, sp1, sp2){
   }
   desc
 }
+
+
 # a list of the descendants from that common ancestor
 desc <- get_clade(ape$phy, "Chlorurus_sordidus", "Hipposcarus_longiceps")
 regimes <- rep(1, length(ape$phy$edge[,1])) 
-regimes[desc] <- 2
+regimes[desc] <- 0
 
 # use to define regimes
 colors <- rep("black", length(ape$phy$edge[,1]))
@@ -99,31 +104,31 @@ parrotfish_traits <- data.frame(Reg=as.integer(reg_tips), row.names=ape$phy$tip.
 rownames(traits) <- traits$Row.names
 trait_data <- merge(parrotfish_traits, traits[,-1], by="row.names")
 names(trait_data)[1] <- "Genus_species"
-trait_data[[1]] <- as.factor(trait_data[[1]])
+
 # Get just the active trait # not sure if necessary
-parrotfish_trait <- trait_data[c("Genus_species", "Reg", "gape.y")]
-#names(parrotfish_trait)[3] <- "X" # not sure if necessary
+parrotfish_trait <- trait_data[c("Genus_species", "Reg", "prot.y")]
 
 paste("starting OUwie")
 
 require(OUwie)
-oumv <- OUwie(ape$phy,parrotfish_trait,model=c("OUMV"),root.station=TRUE, plot.resid=FALSE)
-ouma <- OUwie(ape$phy,parrotfish_trait,model=c("OUMA"),root.station=TRUE, plot.resid=FALSE)
-oumva <- OUwie(ape$phy,parrotfish_trait,model=c("OUMVA"),root.station=TRUE, plot.resid=FALSE)
+oumv  <- OUwie(ape$phy, parrotfish_trait, model = c("OUMV"),  
+               root.station=TRUE, plot.resid=FALSE)
+ouma  <- OUwie(ape$phy, parrotfish_trait, model = c("OUMA"),
+               root.station=TRUE, plot.resid=FALSE)
+oumva <- OUwie(ape$phy, parrotfish_trait, model = c("OUMVA"),
+               root.station=TRUE, plot.resid=FALSE)
 
 
 
 
 
 
-
-#
 ### Check that order doesn't matter (tree listing labels in different order than trait)
 #data(tworegime)
 #owie1 <- OUwie(tree,trait,model=c("OUMV"),root.station=FALSE, plot.resid=TRUE)
 #trait <- trait[with(trait, order(X)),]
 #owie2 <- OUwie(tree,trait,model=c("OUMV"),root.station=FALSE, plot.resid=TRUE)
-#identical(owie1$Param.est, owie2$Param.est)
+#identical(owie1$Param.est, owie2$Param.est) # Yup, checks out
 #
 
 
---
+++ demo/method2_tools.R
@@ -0,0 +1,79 @@
+
+#' returns a phylogenetic tree painted for ouwie and data
+#' @param phy a phylogenetic tree of class phylo (ape)
+#' @param clades: a list of clades defined by species pairs,
+#' e.g. list(c(sp1, sp2), c(sp3, sp4)), or just a vector c(sp1, sp2)
+#' the descendents of the MRCA of sp1 and sp2 define the regime
+paint_phy <- function(phy, data, clades, show_plot=TRUE){
+
+ # drop unmatched tips & characters
+  pruned <- treedata(phy, data)
+  phy <- pruned$phy
+
+  # one regime or multiple?
+  if(is.list(clades)){
+    sp1 <- sapply(clades, function(x) x[1])
+    sp2 <- sapply(clades, function(x) x[1])
+    n <- length(clades)
+  } else if(is.character){
+    sp1 <- clades[1]
+    sp2 <- clades[2]
+    n <- 1
+  }
+
+  desc <- vector("list", length=n)
+  regimes <- rep(0, length(phy$edge[,1]))
+
+  for(i in 1:n){
+    # Create the painting
+    desc[[i]] <- get_clade(phy, sp1[i], sp2[i])
+    regimes[desc[[i]]] <- i
+  }
+
+  # pick out tips and nodes
+  tips <-  !(phy$edge[,2] %in% phy$edge[,1]) 
+  regime_tips <- regimes[tips]
+  regime_nodes <- c(0, regimes[!tips]) # adds root back on
+
+  # stick them on the tree
+  phy$node.label <- as.character(regime_nodes)
+
+  Reg <- data.frame(Reg=as.integer(regime_tips),
+                    row.names=phy$tip.label)
+  data <- merge(Reg, as.data.frame(pruned$data), by="row.names")
+  names(data)[1] <- "Genus_species"
+
+  # optionally plot the tree to verify the painting
+  if(show_plot){
+    colors <- rep("black", length(phy$edge[,1]))
+    colors[desc] <- "red"
+    plot(phy, edge.color=colors)
+    nodelabels(pch=21, bg=c("black", colors[!tips]))
+    tiplabels(pch=21, bg=colors[tips])
+  }
+
+  list(phy=phy, data=data)
+}
+
+
+#' A function that returns the edge numbers of all branches 
+#' descended from the mrca of sp1 and sp2.  
+#' @param phy a phylogenetic tree of class phylo (ape)
+#' @param sp1 the tip.label of the 1st species
+#' @param sp1 the tip.label of the 2nd species 
+#' the descendents of the MRCA of sp1 and sp2 define the regime
+get_clade <- function(phy, sp1, sp2){
+  M <- mrca(phy)
+  who <- M[sp1, sp2]
+  desc <- which(phy$edge[,1] %in% who)
+  l1 <- length(desc)
+  l2 <- length(desc)+1
+  while(l1!=l2){
+    l1 <- length(desc)
+    who <- phy$edge[desc, 2]
+    desc <- unique(c(desc, which(phy$edge[,1] %in% who)))
+    l2 <- length(desc)
+  }
+  desc
+}
+
