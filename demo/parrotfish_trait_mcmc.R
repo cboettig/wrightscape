dfe9559b2a4bd8872697aa5fc2ea890336df7d0c
Carl Boettiger <cboettig@gmail.com>
2011-10-28 16:27:57 -0700

    trying to get the mcmc running consistently

--- demo/parrotfish_trait_mcmc.R
+++ demo/parrotfish_trait_mcmc.R
@@ -7,18 +7,20 @@ source("parrotfish_data.R")
 MaxTime = 1e6
 spec = list(alpha="fixed", sigma="indep", theta="global")
 trait <-  "open"
-nchains <- 10
+nchains <- 1
 burnin <- 1:1e3
-
-sfInit(par=T, cpu=nchains)
-sfLibrary(wrightscape)
-sfExportAll()
 
 
 # START SMART PLEASE
 start <- multiTypeOU(data=labrid$data[trait], tree=labrid$tree, 
 regimes=intramandibular, model_spec=spec) #,
 #                  method ="SANN", control=list(maxit=100000,temp=50,tmax=20))
+
+
+#sfInit(par=T, cpu=nchains)
+sfLibrary(wrightscape)
+sfExportAll()
+
 
 o <- sfLapply(1:nchains, function(i){
 chains <- phylo_mcmc(labrid$data[trait], labrid$tree, intramandibular,
