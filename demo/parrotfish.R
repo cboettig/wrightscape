ca8108ea883f5847f846edf458cda336947ff41c
Carl Boettiger <cboettig@gmail.com>
2011-11-17 14:37:40 -0800

    ggplot graphs yay

--- demo/parrotfish.R
+++ demo/parrotfish.R
@@ -7,9 +7,9 @@ require(ggplot2)
 data(parrotfish)
 
 #traits <- c("bodymass", "close", "open", "kt", "gape.y",  "prot.y", "AM.y", "SH.y", "LP.y")
-traits <- c("close", "open")
+traits <- c("close", "open", "gape.y",  "prot.y")
 
-sfInit(par=T, 2)    # for debugging locally
+sfInit(par=T, 4)    # for debugging locally
 sfLibrary(wrightscape)
 sfExportAll()
 fits <- sfLapply(traits, function(trait){
@@ -23,15 +23,28 @@ fits <- sfLapply(traits, function(trait){
     control=list(maxit=5000))  
   sigmas_out <- replicate(20, bootstrap(sigmas))
 
-  list(ouma=alphas_out, oumv=sigmas_out)
+  full <- multiTypeOU(data=dat[trait], tree=tree, regimes=intramandibular, 
+    model_spec = list(alpha = "global", sigma = "indep", theta = "indep"), 
+    control=list(maxit=5000))  
+  full_out <- replicate(20, bootstrap(full))
+
+  list(ouma=alphas_out, oumv=sigmas_out, oumva=full_out)
 })
+
 names(fits) <- traits
 data <- melt(fits)
 names(data) <- c("regimes", "param", "rep", "value", "model", "trait")
 
+p1 <- ggplot(subset(data,  param=="loglik")) + geom_boxplot(aes(model, value)) + facet_wrap(~ trait, scales="free_y")
+p2 <- ggplot(subset(data, value < 100 & param %in% c("sigma", "alpha", "theta"))) + geom_boxplot(aes(trait, value, fill=regimes)) + facet_grid(param ~ model, scales = "free") 
 
-p <- ggplot(subset(data, value < 20 & param %in% c("sigma", "alpha", "theta"))) + geom_boxplot(aes(trait, value, fill=regimes)) + facet_grid(param ~ model) 
-print(p)
+save(list=ls(), file="parrotfish.Rdat")
+
+#ggsave("parrotfish_lik.png", p1)
+#ggsave("parrotfish_params.png", p2)
+#require(socialR)
+#upload("parrotfish_*.png", script="parrotfish.R", tag="phylogenetics")
+
 
 #p <- ggplot(subset(data, param=="alpha" & value < 100)) + geom_boxplot(aes(trait, value, fill=regimes)) + facet_grid(. ~ model) 
 #ggplot(data) + geom_boxplot(aes(trait, value, fill=regimes)) + facet_wrap(param~.)
