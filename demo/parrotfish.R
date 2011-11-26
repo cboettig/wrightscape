fe04e814b668ceaf61b59cd2be01ec2fb4a67145
Carl Boettiger <cboettig@gmail.com>
2011-11-25 16:32:45 -0800

    parrotfish.R  autocommit

--- demo/parrotfish.R
+++ demo/parrotfish.R
@@ -4,45 +4,62 @@ require(wrightscape)
 require(snowfall)
 require(ggplot2)
 
-data(parrotfish)
+# store the unique id of this script version
+require(socialR)
+gitaddr <- gitcommit("parrotfish.R")
+id <- gitlog()$shortID
 
+
+
+data(parrotfish)
 #traits <- c("bodymass", "close", "open", "kt", "gape.y",  "prot.y", "AM.y", "SH.y", "LP.y")
 traits <- c("close", "open", "gape.y",  "prot.y")
 
 sfInit(par=T, 4)    # for debugging locally
 sfLibrary(wrightscape)
 sfExportAll()
-fits <- sfLapply(traits, function(trait){
-  alphas <- multiTypeOU(data=dat[trait], tree=tree, regimes=intramandibular, 
-   model_spec = list(alpha = "indep", sigma = "global", theta = "indep"),
-   control=list(maxit=5000)) 
-  alphas_out <- replicate(50, bootstrap(alphas))
+fits <- lapply(traits, function(trait){
 
-  sigmas <- multiTypeOU(data=dat[trait], tree=tree, regimes=intramandibular, 
-    model_spec = list(alpha = "global", sigma = "indep", theta = "indep"), 
-    control=list(maxit=5000))  
-  sigmas_out <- replicate(50, bootstrap(sigmas))
+  # declare function for shorthand
+  multi <- function(modelspec, reps = 20){
+    m <- multiTypeOU(data = dat[trait], tree = tree, regimes = intramandibular, 
+  		     model_spec = modelspec, 
+#		     control = list(temp = 20, tmax = 50), method = "SANN"
+		     control = list(maxit=5000)
+		    ) 
+    replicate(reps, bootstrap(m))
+  }
 
-  full <- multiTypeOU(data=dat[trait], tree=tree, regimes=intramandibular, 
-    model_spec = list(alpha = "global", sigma = "indep", theta = "indep"), 
-    control=list(maxit=5000))  
-  full_out <- replicate(50, bootstrap(full))
+  bm <- multi(list(alpha = "fixed", sigma = "indep", theta = "global"))
+  s1 <- multi(list(alpha = "global", sigma = "indep", theta = "global")) 
+  a1  <- multi(list(alpha = "indep", sigma = "global", theta = "global")) 
+  s2 <- multi(list(alpha = "global", sigma = "indep", theta = "indep")) 
+  a2  <- multi(list(alpha = "indep", sigma = "global", theta = "indep")) 
 
-  list(ouma=alphas_out, oumv=sigmas_out, oumva=full_out)
+  list(bm=bm, s1=s1, a1=a1, s2=s2, a2=a2)
 })
 
-names(fits) <- traits
+# Reformat and label data for plotting
+names(fits) <- traits  # each fit is a different trait (so use it for a label)
 data <- melt(fits)
 names(data) <- c("regimes", "param", "rep", "value", "model", "trait")
 
-p1 <- ggplot(subset(data,  param=="loglik")) + geom_boxplot(aes(model, value)) + facet_wrap(~ trait, scales="free_y")
-p2 <- ggplot(subset(data, value < 100 & param %in% c("sigma", "alpha", "theta"))) + geom_boxplot(aes(trait, value, fill=regimes)) + facet_grid(param ~ model, scales = "free") 
+p1 <- ggplot(subset(data,  param=="loglik")) + 
+      geom_boxplot(aes(model, value)) +
+      facet_wrap(~ trait, scales="free_y")
 
-save(list=ls(), file="parrotfish.Rdat")
-ggsave("parrotfish_lik.png", p1)
-ggsave("parrotfish_params.png", p2)
+p2 <- ggplot(subset(data, value < 100 & param %in% c("sigma", "alpha", "theta"))) +
+      geom_boxplot(aes(trait, value, fill=regimes)) + 
+      facet_grid(param ~ model, scales = "free") 
+
+save(list=ls(), file=sprintf("%s.Rdat", id))
+ggsave(sprintf("%s_lik.png", id), p1)
+ggsave(sprintf("%s_params.png", id),  p2)
+
+
 require(socialR)
-upload("parrotfish_lik.png", script="parrotfish.R", tag="phylogenetics")
+#load(file=sprintf("%s.Rdat", id))
+#upload(sprintf("%s_*.png", id), gitaddr=gitaddr, tag="phylogenetics")
 
 
 #p <- ggplot(subset(data, param=="alpha" & value < 100)) + geom_boxplot(aes(trait, value, fill=regimes)) + facet_grid(. ~ model) 
