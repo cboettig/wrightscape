3da057ba8d84717bd5e2b0a33b84aaacfaf96306
Carl Boettiger <cboettig@gmail.com>
2011-11-29 15:22:50 -0800

    unit_test5, lambda parameter illustrates the problem

--- demo/unit_test2.R
+++ demo/unit_test2.R
@@ -1,8 +1,50 @@
 # unit_test2.R
 rm(list=ls())
-
 require(wrightscape)
 data(labrids)
+regimes <- intramandibular
+
+# test of the multi-case
+test <- dat[["prot.y"]]
+group_1 <- (regimes==levels(regimes)[1]) & !is.na(test)
+group_2 <- (regimes==levels(regimes)[2]) & !is.na(test)
+test[group_1] <- rnorm(sum(group_1), sd=10)
+test[group_2] <- rnorm(sum(group_2), sd=1)
+
+# Load internal functions
+require(devtools)
+load_all("..")
+lca <- lca_calc(tree)
+
+sigma <- c(10,10)
+alpha <- c(sigma[1]^2/(2*1), sigma[2]^2/(2*10))
+
+theta <- c(mean(test[regimes==levels(regimes)[1]], na.rm=T), 
+	   mean(test[regimes==levels(regimes)[2]], na.rm=T))
+Xo <- theta[match(regimes[1], levels(regimes))]  
+
+# expect higher alpha on "other" (alpha2) to be best, higher on alpha1 is worst
+even <- multiOU_lik_lca(test, tree, regimes,  alpha=mean(alpha), sigma=sigma, theta=theta, Xo=Xo, lca=lca_calc(tree))
+bad <- multiOU_lik_lca(test, tree, regimes, alpha=c(alpha[2], alpha[1]), sigma=sigma, theta=theta, Xo=Xo, lca=lca_calc(tree))
+good <- multiOU_lik_lca(test, tree, regimes, alpha=alpha, sigma=sigma, theta=theta, Xo=Xo, lca=lca_calc(tree) )
+
+good > bad
+# BUT NO!  FIXME -- check out means of the two groups!!
+
+
+multiOU_lik_lca(testcase, tree, intramandibular, alpha=c(2,2), sigma=c(5,5), theta=c(0.5, 0.5), Xo=0.5, lca)
+multiOU_lik_lca(testcase, tree, intramandibular, alpha=c(5,2), sigma=c(5,5), theta=c(0.5, 0.5), Xo=0.5, lca)
+multiOU_lik_lca(testcase, tree, intramandibular, alpha=c(2,5), sigma=c(5,5), theta=c(0.5, 0.5), Xo=0.5, lca)
+
+## MODEL FIT
+a1_spec  <- list(alpha = "indep", sigma = "global", theta = "global")
+a1 <- multiTypeOU(data=testcase, tree=tree, regimes=intramandibular, model_spec=a1_spec)
+a1$alpha
+
+
+
+
+
 
 # Test of single OU
 test <- dat[["prot.y"]]
@@ -13,28 +55,5 @@ test[!is.na(test)] <- rnorm(sum(!is.na(test)), sd=.1)
 m2 <- multiTypeOU(data=test, tree=tree, regimes=intramandibular, model_spec=ou)
 m1$alpha # larger variance has smaller alpha
 m2$alpha
-
-
-# test of the multi-case
-testcase <- dat[["prot.y"]]
-other.prot.data <- testcase[intramandibular=="other" & !is.na(testcase)]
-intra.prot.data <- testcase[intramandibular!="other" & !is.na(testcase)] 
-testcase[intramandibular=="other" & !is.na(testcase)] <- rnorm(length(other.prot.data), sd=.1)
-testcase[intramandibular!="other" & !is.na(testcase)] <- rnorm(length(intra.prot.data), sd=10)
-# Load internal functions
-require(devtools)
-load_all("..")
-data <- testcase
-lca <- lca_calc(tree)
-
-# expect higher alpha on "other" (alpha2) to be best, higher on alpha1 is worst
-# BUT NO!  FIXME -- check out means of the two groups!!
-multiOU_lik_lca(testcase, tree, intramandibular, alpha=c(2,2), sigma=c(5,5), theta=c(0.5, 0.5), Xo=0.5, lca)
-multiOU_lik_lca(testcase, tree, intramandibular, alpha=c(5,2), sigma=c(5,5), theta=c(0.5, 0.5), Xo=0.5, lca)
-multiOU_lik_lca(testcase, tree, intramandibular, alpha=c(2,5), sigma=c(5,5), theta=c(0.5, 0.5), Xo=0.5, lca)
-
-a1_spec  <- list(alpha = "indep", sigma = "global", theta = "global")
-a1 <- multiTypeOU(data=testcase, tree=tree, regimes=intramandibular, model_spec=a1_spec)
-a1$alpha
 
 
--- demo/unit_test5.R
+++ demo/unit_test5.R
@@ -1,32 +1,41 @@
 #unit_test5.R
 
 # Try on a star tree where we can use analytic results...
-rm(list=ls())
 require(wrightscape)
 require(geiger)
-data(labrids)
-tree <- convert(lambdaTree(convert(tree), .01))
+#data(labrids)
+load("../data/labrids.rda")
+tree <- convert(lambdaTree(convert(tree), .1))
 test <- dat[["prot.y"]]
 regimes <- pharyngeal
 levels(regimes) <- 1:length(levels(regimes))
 wrasse <- !is.na(test) & (regimes == 1)
 parrotfish <- !is.na(test) & (regimes == 2)
-test[wrasse] <- rnorm(sum(wrasse), sd=1)
-test[parrotfish] <- rnorm(sum(parrotfish), sd=10)
+test[wrasse] <- rnorm(sum(wrasse), sd=1, mean=0)
+test[parrotfish] <- rnorm(sum(parrotfish), sd=10, mean=0)
 theta <- c(mean(test[regimes==1], na.rm=T), mean(test[regimes==2], na.rm=T))
 Xo <- theta[match(regimes[1], levels(regimes))]  
 # Should be approximately the optimum likelihood (along the alpha-sigma ridge)
-sigma <- c(15,15)
+sigma <- c(10,10)
 alpha <- c(sigma[1]^2/(2*1), sigma[2]^2/(2*10))
 
 require(devtools)
 load_all("..")
-multiOU_lik_lca(test, tree, regimes, alpha=alpha, sigma=sigma, theta=theta, Xo=Xo, lca=lca_calc(tree))
+good <- multiOU_lik_lca(test, tree, regimes, alpha=alpha, sigma=sigma, theta=theta, Xo=Xo, lca=lca_calc(tree))
+bad <- multiOU_lik_lca(test, tree, regimes, alpha=rev(alpha), sigma=sigma, theta=theta, Xo=Xo, lca=lca_calc(tree))
+print(good > bad)
 
 
+a1 <- list(alphas="indep", sigmas="global", thetas="global")
+a2 <- list(alphas="indep", sigmas="global", thetas="indep")
+m1 <- multiTypeOU(test, tree, regimes, alpha=alpha, sigma=sigma, theta=theta, Xo=Xo, model_spec=a2)
+print(m1$alpha) # YAY THIS IS CORRECT!!
 
-ou  <- list(alpha = "global", sigma = "global", theta = "global")
-m1 <- multiTypeOU(data=test, tree=tree, regimes=regimes)
+m2 <- multiTypeOU(test, tree, regimes, model_spec=a2)
+print(m2$alpha)
 
+
+m3 <-  multiTypeOU(test, tree, regimes, alpha=alpha, sigma=sigma, theta=theta, Xo=Xo)
+print(m3$alpha)
 
 
