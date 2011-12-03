a1c6e40173dd8cfb972d95685188bc0a2fc8429b
Carl Boettiger <cboettig@gmail.com>
2011-12-02 17:07:30 -0800

    labrids.R  autocommit

--- demo/labrids.R
+++ demo/labrids.R
@@ -27,7 +27,7 @@ fits <- sfLapply(traits, function(trait){
 
   # declare function for shorthand
   multi <- function(modelspec, reps = 20){
-    m <- multiTypeOU(data = dat[trait], tree = tree, regimes = pharyngeal, 
+    m <- multiTypeOU(data = dat[trait], tree = tree, regimes = intramandibular, 
   		     model_spec = modelspec, 
 		     control = list(maxit=3000)
 		    ) 
@@ -48,24 +48,52 @@ names(fits) <- traits  # each fit is a different trait (so use it for a label)
 data <- melt(fits)
 names(data) <- c("regimes", "param", "rep", "value", "model", "trait")
 
+
+
+
 #model likelihood
 p1 <- ggplot(subset(data,  param=="loglik")) + 
       geom_boxplot(aes(model, value)) +
       facet_wrap(~ trait, scales="free_y")
 
-# paramater estimates
-p2 <- ggplot(subset(data, param %in% c("sigma", "alpha"))) +
-      geom_boxplot(aes(trait, value, fill=regimes)) + 
-      facet_grid(param ~ model, scales = "free") + scale_y_log() 
+p2 <-  ggplot(subset(data, param %in% c("sigma", "alpha") & model != "bm"),
+              aes(model, value, fill=regimes)) + 
+#       stat_summary(fun.y=mean, geom="bar", position="dodge") + # add bars for some extra ink...
+       stat_summary(fun.data=median_hilow, geom="pointrange", aes(color=regimes), 
+                    position = position_dodge(width=0.90), conf.int=.5) +
+#       scale_y_log() + 
+       facet_grid(param ~ trait, scales = "free_y") + 
+       coord_cartesian(ylim=c(0,10), wise=TRUE)
 
-p3 <- ggplot(subset(data, param %in% c("sigma", "alpha"))) +
-      geom_boxplot(aes(model, value, fill=regimes)) + 
-      facet_grid(trait ~ param, scales = "free") 
+
+## Just plot the parameters seperately
+p3 <-  ggplot(subset(data, param %in% c("sigma") ), 
+              aes(model, value, fill=regimes)) + 
+       stat_summary(fun.y=mean, geom="bar", position="dodge", alpha=.5) + # add bars for some extra ink...
+       stat_summary(fun.data=median_hilow, geom="pointrange", aes(color=regimes), 
+                    position = position_dodge(width=0.90), conf.int=.5) +
+#       scale_y_log() + 
+       facet_grid(param ~ trait, scales = "free_y") + 
+       coord_cartesian(ylim=c(0,4), wise=TRUE) +  # easiest to just adjust the zoom limits manually still...
+       opts(title="sigma")
+
+p4 <-  ggplot(subset(data, param %in% c("alpha") ), 
+              aes(model, value, fill=regimes)) + 
+       stat_summary(fun.y=mean, geom="bar", position="dodge", alpha=.5) + # add bars for some extra ink...
+       stat_summary(fun.data=median_hilow, geom="pointrange", aes(color=regimes), 
+                    position = position_dodge(width=0.90), conf.int=.5) +
+#       scale_y_log() + 
+       facet_grid(param ~ trait, scales = "free_y") + 
+       coord_cartesian(ylim=c(0,10), wise=TRUE) +
+        opts(title = "alpha")
+
+
 
 save(list=ls(), file=sprintf("%s.Rdat", id))
 ggsave(sprintf("%s_lik.png", id), p1)
 ggsave(sprintf("%s_params_p2.png", id),  p2)
 ggsave(sprintf("%s_params_p3.png", id),  p3)
+ggsave(sprintf("%s_params_p4.png", id),  p4)
 
 
 print(id)
