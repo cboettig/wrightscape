060da465135d19d8bb30e6db6b74fd3e84e891ca
Carl Boettiger <cboettig@gmail.com>
2011-12-08 09:50:45 -0800

    labrid_power.R  autocommit

--- demo/labrid_power.R
+++ demo/labrid_power.R
@@ -15,7 +15,7 @@ print(id)
 data(labrids)
 
 traits <- c("bodymass", "close", "open", "kt", "gape.y",  "prot.y", "AM.y", "SH.y", "LP.y")
-regimes <- two_shifts
+regimes <- pharyngeal 
 
   # declare function for shorthand
 sfInit(par=F)    # for debugging locally
@@ -32,7 +32,7 @@ fits <- lapply(traits, function(trait){
 
   mc <- montecarlotest(bm,a2)
   png(paste(trait, "_mc_labrid_", id, ".png", sep=""))
-    plot(mc,show_data=TRUE, main=paste(trait, levels(regimes)))
+    plot(mc,show_data=TRUE, main=paste(trait, "pharyngeal"))
   dev.off()
   list(mc$null_dist, mc$test_dist, -2*(mc$null$loglik-mc$test$loglik))
 })
