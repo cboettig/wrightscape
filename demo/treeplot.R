29569de3ce419131b67f7dbd83b60f27fad6c8d6
Carl Boettiger <cboettig@gmail.com>
2012-05-03 18:24:49 -0700

    working on figures

--- demo/treeplot.R
+++ demo/treeplot.R
@@ -9,9 +9,20 @@
 
 # plot the phylogeny
 rm(list=ls())
-source("method2_tools.R")
+source("method2/method2_tools.R")
+
+require(wrightscape)
+require(geiger)
+data(labrids)
+
+row.names(ape.dat) <- ape.dat$Row.names
 
 myphy <- paint_phy(ape.phy, ape.dat, list(c("Bolbometopon_muricatum", "Sparisoma_radians"), c("Chlorurus_sordidus", "Hipposcarus_longiceps")))
+
+
+myphy$colors <- gsub("1", "blue",  myphy$colors)
+myphy$colors <- gsub("2", "green",  myphy$colors)
+myphy$colors <- gsub("3", "red",  myphy$colors)
 
 cairo_pdf(file="phylo.pdf")
 plot(myphy$phy, edge.color=myphy$colors, type="fan", show.tip.label=FALSE, edge.width=2)
